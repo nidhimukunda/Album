e0ddf45b3d7ac4f4938fe2d86f97b2f470a429ce
nidhimukunda <nidhimukunda@gmail.com>
2020-03-12 04:41:31 +0000

    Final commit

--- src/app/image/shared/image.service.ts
+++ src/app/image/shared/image.service.ts
@@ -8,16 +8,16 @@ export class ImageService {
     }
 
     getImage(id:number) {
-        return IMAGES.slice(0).find(image => image.id == id); 
+        return IMAGES.slice(0).find(image => image.id == id);
     }
 }
 
 const IMAGES = [
-    {"id": 1, "category": "pixel", "caption": "Google Pixel 2 XL", "url": "assets/img/pixel.jpg"},
+    {"id": 6, "category": "dslr", "caption": "Canon 5D Mark III", "url": "assets/img/canon.jpg"},
     {"id": 2, "category": "iphone", "caption": "Apple iPhone X", "url": "assets/img/iphone.jpg"},
     {"id": 3, "category": "macbook", "caption": "Apple Macbook", "url": "assets/img/macbook.jpg"},
-    {"id": 4, "category": "xbox", "caption": "Microsoft Xbox One X", "url": "assets/img/xbox.jpg"},
+    {"id": 1, "category": "pixel", "caption": "Google Pixel 2 XL", "url": "assets/img/pixel.jpg"},
     {"id": 5, "category": "playstation", "caption": "Sony Playstation 4", "url": "assets/img/ps4.jpg"},
-    {"id": 6, "category": "dslr", "caption": "Canon 5D Mark III", "url": "assets/img/canon.jpg"}
-    
+    {"id": 4, "category": "xbox", "caption": "Microsoft Xbox One X", "url": "assets/img/xbox.jpg"}
+
 ]
